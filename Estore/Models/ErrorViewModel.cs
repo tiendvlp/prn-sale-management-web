8e07933861dabf7387888b0c02d713ea00b2a7e3
tiendvlp <tiendvlp@gmail.com>
2021-10-28 23:53:27 +0700

    init

---
+++ Estore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Estore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
